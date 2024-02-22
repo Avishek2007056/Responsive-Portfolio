f03719c64c380e1128c6f314742f064e86cf6eaf
Sajib <p2385553@gmail.com>
2024-02-23 00:56:44 +0600

    frontend files

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -15,4 +35,33 @@ window.onscroll = () => {
             });
         };
     });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+
+
 };
+
+ScrollReveal({
+    //  reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200
+ });
+
+ ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ ScrollReveal().reveal('.home-img, .experiences-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+
+ const typed = new Typed('.multiple-text', {
+    strings: ['Student', 'Programmer', 'Commedian'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+ });
